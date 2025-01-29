38ba8bf9fc19bc491c4b1b5b3f142cd283f483f7
rustam <rustam.gabidullin16@gmail.com>
2025-01-29 15:18:42 +0300

    sss

--- proekt.py
+++ proekt.py
@@ -1,11 +1,4 @@
-from huggingface_hub import InferenceClient
 import streamlit as st
-from mistral_inference.transformer import Transformer
-from mistral_inference.generate import generate
-from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
-from mistral_common.protocol.instruct.messages import UserMessage
-from mistral_common.protocol.instruct.request import ChatCompletionRequest
-
 from openai import OpenAI
 
 client = OpenAI(
